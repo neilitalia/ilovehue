a554021a8d1e0d1702997c5bb25c2080b05f554e
Neil Italia <neilitalia1998@gmail.com>
2021-08-09 14:15:11 -0500

    :sparkles: Add instructions page

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const nameInput = document.querySelector('input.name-input')
+const nameDisplay = document.querySelector('h3.name-display')
+const scoreDisplay = document.querySelector('h3.score-display')
 const acceptButton = document.querySelector('button.accept-button')
 let cookies = null
 
@@ -12,12 +14,28 @@ const formatCookiesToObj = () => {
   console.log('cookies :>> ', cookies)
 }
 
+const updateNameDisplay = () => {
+  if (cookies.visited) {
+    nameDisplay.innerHTML = `Welcome back, ${cookies.name}`
+  } else {
+    nameDisplay.innerHTML = `Hey there new user!`
+  }
+}
+
+const updateScoreDisplay = () => {
+  if (cookies.visited) {
+    scoreDisplay.innerHTML = `Your score: ${cookies.score}`
+  } else {
+    scoreDisplay.innerHTML = `Your score: 0`
+  }
+}
+
 const acceptCookiesAndName = () => {
   if (cookies.visited) {
-    console.log(`Welcome back, ${cookies.name}`)
+    nameDisplay.innerHTML = `Welcome back, ${cookies.name}`
   } else {
-    console.log('new visitor')
     const name = nameInput.value
+    nameDisplay.innerHTML = `Welcome, ${name}!`
     document.cookie = `name=${name}; max-age=${30 * 24 * 60 * 60};`
     document.cookie = `visited=true; max-age=${30 * 24 * 60 * 60};`
     document.cookie = `score=0; max-age=${30 * 24 * 60 * 60};`
@@ -27,6 +45,8 @@ const acceptCookiesAndName = () => {
 window.addEventListener('load', () => {
   cookies = document.cookie
   formatCookiesToObj()
+  updateNameDisplay()
+  updateScoreDisplay()
 })
 
 acceptButton.addEventListener('click', () => {
