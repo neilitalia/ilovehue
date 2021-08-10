dc25ed71ccd5916b8184474855eb26ba204e0e50
Neil Italia <neilitalia1998@gmail.com>
2021-08-10 14:34:10 -0500

    :art: Improve getPuzzles data structure

--- js/puzzles.js
+++ js/puzzles.js
@@ -1,39 +1,57 @@
 const getPuzzle = (puzzleName) => {
-  const summer = [
-    '#57C2DA',
-    '#7BA683',
-    '#878D36',
-    '#9BD4E7',
-    '#A0BA8B',
-    '#9EA436',
-    '#D5E8F5',
-    '#C7D194',
-    '#B8BF33'
-  ]
+  const summer = {
+    name: 'summer',
+    difficulty: 'easy',
+    gridSize: 3,
+    tileSize: '150px',
+    board: [
+      '#57C2DA',
+      '#7BA683',
+      '#878D36',
+      '#9BD4E7',
+      '#A0BA8B',
+      '#9EA436',
+      '#D5E8F5',
+      '#C7D194',
+      '#B8BF33'
+    ]
+  }
 
-  const autumn = [
-    '#C24141',
-    '#793A2C',
-    '#3D260F',
-    '#D8846B',
-    '#A4634B',
-    '#78472F',
-    '#F4C9A3',
-    '#D89571',
-    '#C2694A'
-  ]
+  const autumn = {
+    name: 'autumn',
+    difficulty: 'easy',
+    gridSize: 3,
+    tileSize: '150px',
+    board: [
+      '#C24141',
+      '#793A2C',
+      '#3D260F',
+      '#D8846B',
+      '#A4634B',
+      '#78472F',
+      '#F4C9A3',
+      '#D89571',
+      '#C2694A'
+    ]
+  }
 
-  const winter = [
-    '#276574',
-    '#1F3C4C',
-    '#0E1327',
-    '#7F9880',
-    '#637C76',
-    '#496269',
-    '#DBE686',
-    '#BADDAB',
-    '#96D5CD'
-  ]
+  const winter = {
+    name: 'winter',
+    difficulty: 'easy',
+    gridSize: 3,
+    tileSize: '150px',
+    board: [
+      '#276574',
+      '#1F3C4C',
+      '#0E1327',
+      '#7F9880',
+      '#637C76',
+      '#496269',
+      '#DBE686',
+      '#BADDAB',
+      '#96D5CD'
+    ]
+  }
 
   switch (puzzleName) {
     case 'summer':
@@ -46,6 +64,7 @@ const getPuzzle = (puzzleName) => {
       return winter
       break
     default:
+      return ['summer', 'autumn', 'winter']
       break
   }
 }
